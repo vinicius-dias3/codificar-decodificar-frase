84a44a436a54ccf6e1e91229c8ae3c06ac11ec82
vinicius-dias3 <vinicius.sod3@gmail.com>
2023-08-04 17:52:20 -0300

    corrigir nome de classe/variaveis e responsivo

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
+//CORRIGIR NOME DAS VARIÁVEIS
 const botaoCodificar = document.querySelector('.btn-codificar')
 const botaoDecodificar = document.querySelector('.btn-decodificar')
-// const botaoCopiar = document.querySelector('.btn-copiar')
-let conteudoFraseCodificada
+const botaoCopiar = document.querySelector('.btn-copiar')
+
+let fraseResultado = document.querySelector('.resultado p')
+let saidaFrasePronta
 let conteudoFraseDecodificada
 const substituicoesCodificar = {
     'a': 'ai',
@@ -16,7 +19,6 @@ const substituicoesCodificar = {
 botaoCodificar.addEventListener('click', (event) =>{
     event.preventDefault()
     const frase = document.querySelector('textarea').value
-    let fraseResultado = document.querySelector('.resultado p')
     const arrayDaFrase = frase.split('')
     // console.log(arrayDaFrase)
     const arrayCodificada = []
@@ -26,16 +28,16 @@ botaoCodificar.addEventListener('click', (event) =>{
         arrayCodificada.push(substituicao || letra)
     })
     // console.log(arrayCodificada.join('')) //codificando
-    conteudoFraseCodificada = arrayCodificada.join('')
+    saidaFrasePronta = arrayCodificada.join('')
     // console.log(conteudoFraseCodificada) //funcionando
-    fraseResultado.innerHTML = conteudoFraseCodificada
+    fraseResultado.innerHTML = saidaFrasePronta
 })   
 
 botaoDecodificar.addEventListener('click', (event)=> {
     event.preventDefault()
     const frase = document.querySelector('textarea').value
-    console.log(frase)
-    const fraseDecodificada = frase.replace(/un/g, 'l')
+    // console.log(frase)
+    fraseDecodificada = frase.replace(/un/g, 'l')
     .replace(/wq/g, 's')
     .replace(/oo/g, 'u')
     .replace(/bi/g, 'o')
@@ -43,21 +45,17 @@ botaoDecodificar.addEventListener('click', (event)=> {
     .replace(/ey/g, 'e')
     .replace(/ai/g, 'a')
     // console.log(fraseDecodificada) //funcionando
-    let fraseResultado = document.querySelector('.resultado p')
     fraseResultado.innerHTML = fraseDecodificada
+    saidaFrasePronta = fraseDecodificada
+    console.log(saidaFrasePronta)
 })
 
-botaoCopiar.addEventListener('click', ()=> {
-    let fraseParaCopiar = conteudoFraseResultado
-    console.log(fraseCopiada) //copiando frase codificada 
-    
+botaoCopiar.addEventListener('click', async ()=> {
+    const textoParaCopiar = fraseResultado.innerHTML
+    // console.log(textoParaCopiar) //funcionando
+    await navigator.clipboard.writeText(textoParaCopiar)
+    botaoCopiar.classList.add('ativar-animacao')
+    setTimeout(()=>{
+        botaoCopiar.classList.remove('ativar-animacao')
+    }, 1000)
 })
-
-
-/*botaoCopiar.addEventListener('click', ()=> {
-    // console.log(botaoCopiar)
-    let fraseCopiada = conteudoFraseResultado
-    console.log(fraseCopiada) //copiando frase codificado com sucesso
-    // document.frase
-    fraseCopiada.execCommand('copy')
-})*/
